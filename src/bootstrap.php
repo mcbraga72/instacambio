aa9a0cb30c335e96524483b2e3dd5621a935a5c4
Gleidson Brito Santana <gleidsonbrito.ads@gmail.com>
2016-06-24 13:28:21 -0300

    Implementação da utilização do autoloader via Composer.

--- src/bootstrap.php
+++ src/bootstrap.php
@@ -1,14 +1,9 @@
 <?php
 
 if (!defined('ROOT_DIR')) {
-    define('ROOT_DIR', dirname(dirname(__FILE__)));
+    define('ROOT_DIR', dirname(__DIR__));
 }
 if (!defined('DS')) {
     define('DS', DIRECTORY_SEPARATOR);
 }
-require_once ROOT_DIR . DS . 'vendor' . DS . 'autoload.php';
-require_once ROOT_DIR . DS . 'src' . DS . 'Psr4AutoloaderClass.php';
-$autoloaderClass = new Psr4AutoloaderClass();
-$autoloaderClass->register();
-$autoloaderClass->addNamespace('br\com\InstaCambio', ROOT_DIR . DS . 'src' . DS . 'br' . DS . 'com' . DS . 'InstaCambio');
-$autoloaderClass->addNamespace('Goutte\Stub', ROOT_DIR . DS . 'tests' . DS . 'Goutte' . DS . 'Stub');
+require_once ROOT_DIR . DIRECTORY_SEPARATOR . 'vendor' . DIRECTORY_SEPARATOR . 'autoload.php';
